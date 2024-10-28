be37d4b15caaf281009b47d62c30f3ced4c2c8d7
Macauly Vivaldo <134652516+MacaulyV@users.noreply.github.com>
2024-10-28 19:49:23 -0300

    Update CorsConfig.java

--- src/main/java/com/brinquedos/gestaobrinquedos/CorsConfig.java
+++ src/main/java/com/brinquedos/gestaobrinquedos/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 // Configura o CORS para permitir que o frontend (localhost:8080) faça requisições à API
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://toy-control-system.onrender.com"
+                        .allowedOrigins("http://localhost:8080",
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Define quais métodos HTTP são permitidos
                         .allowedHeaders("*") // Permite todos os cabeçalhos
                         .allowCredentials(true); // Permite o envio de credenciais (cookies, autenticação HTTP, etc.)
